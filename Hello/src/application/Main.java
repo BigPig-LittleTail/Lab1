310332c9875774beb1f5ef74673d2bbb9bfc2379
elfSongLonely <434678162.com>
2017-10-22 16:30:49 +0800

    find bugs over

--- Hello/src/application/Main.java
+++ Hello/src/application/Main.java
@@ -1,38 +1,36 @@
 package application;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 
 
 public class Main extends Application {
-	
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            // Read file fxml and draw interface.
-            Parent root = FXMLLoader.load(getClass()
-                    .getResource("/application/MyScence.fxml"));
 
-            primaryStage.setTitle("Lab1-结对编程-有向图");
-            primaryStage.setScene(new Scene(root));
-            primaryStage.show();
-            
-            
-            
-            
+  @Override
+  public void start(Stage primaryStage) {
+    try {
+      // Read file fxml and draw interface.
+      Parent root = FXMLLoader.load(getClass().getResource("/application/MyScence.fxml"));
 
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+      primaryStage.setTitle("Lab1-结对编程-有向图");
+      primaryStage.setScene(new Scene(root));
+      primaryStage.show();
+
+
+
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
-    
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+
 
 }
